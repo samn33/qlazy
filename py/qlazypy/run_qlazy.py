f8d5d3ce85af5c29ff056ddaa9a6b67876e7f934
samn33 <saminriver33@gmail.com>
2021-02-10 22:11:52 +0900

    bug fix

--- py/qlazypy/run_qlazy.py
+++ py/qlazypy/run_qlazy.py
@@ -27,6 +27,7 @@ def run_qlazy_qstate_simulator(qstate, qcirc, cmem, shots=DEF_SHOTS):
     freq = Counter()
     for cnt in range(shots):
         for i, c in enumerate(qcirc):
+            
             if c['kind'] == MEASURE:
                 md = qstate_measure(qstate, {}, qid=c['qid'], shots=1, angle=0.0, phase=0.0, tag=None)
 
@@ -36,6 +37,7 @@ def run_qlazy_qstate_simulator(qstate, qcirc, cmem, shots=DEF_SHOTS):
                         
                 if end_of_measurements == i:
                     freq += md.frequency
+
             else:
                 if c['ctrl'] == None or cmem[c['ctrl']] == 1:
                     qstate_operate_qgate(qstate, kind=c['kind'], qid=c['qid'],
@@ -43,18 +45,21 @@ def run_qlazy_qstate_simulator(qstate, qcirc, cmem, shots=DEF_SHOTS):
 
             # qcirc have only one measurement
             if only_one_measurement_end == True and i == len(qcirc) - 2:
-                md = qstate_measure(qstate, {}, qid=c['qid'], shots=shots, angle=0.0, phase=0.0, tag=None)
+                md = qstate_measure(qstate, {}, qid=qcirc[-1]['qid'], shots=shots, angle=0.0, phase=0.0, tag=None)
                 freq = md.frequency
+                
+                if qcirc[-1]['cid'] != None:
+                    for k,mval in enumerate(list(md.last)):
+                        cmem[qcirc[-1]['cid'][k]] = int(mval)
                 break
-        if only_one_measurement_end == True:
-            break
-        
+            
         # reset classical memory and qubits, if not end of the shots
         if cnt < shots-1:
             cmem = [0] * len(cmem)
             qstate.reset()
 
-    if end_of_measurements > 0:
+    # if end_of_measurements > 0:
+    if measurement_cnt > 0:
         measured_qid = qcirc[end_of_measurements]['qid']
         result = {'measured_qid': measured_qid, 'frequency': freq}
     else:
@@ -83,7 +88,8 @@ def run_qlazy_stabilizer_simulator(stab, qcirc, cmem, shots=DEF_SHOTS):
     freq = Counter()
     for cnt in range(shots):
         for i, c in enumerate(qcirc):
-            if c['kind'] == MEASURE:
+            # if c['kind'] == MEASURE:
+            if c['kind'] == MEASURE and len(qcirc) > 1:
                 mval_list = []
                 for q in c['qid']:
                     mval = stabilizer_measure(stab, q=q)
@@ -97,7 +103,8 @@ def run_qlazy_stabilizer_simulator(stab, qcirc, cmem, shots=DEF_SHOTS):
                         
                 if end_of_measurements == i:
                         freq += f
-            else:
+            # else:
+            elif c['kind'] != MEASURE:
                 if c['ctrl'] == None or cmem[c['ctrl']] == 1:
                     if len(c['qid']) == 1:
                         q0 = c['qid'][0]
@@ -111,16 +118,29 @@ def run_qlazy_stabilizer_simulator(stab, qcirc, cmem, shots=DEF_SHOTS):
                     stabilizer_operate_qgate(stab, kind=c['kind'], q0=q0, q1=q1)
 
             # qcirc have only one measurement at the end
-            if only_one_measurement_end == True and i == len(qcirc) - 2:
-                for _ in range(shots):
-                    stab_tmp = stab.clone()
+            # if only_one_measurement_end == True and i == len(qcirc) - 2:
+            if only_one_measurement_end == True and (i == len(qcirc) - 2 or i == len(qcirc) - 1):
+                for i in range(shots):
+                    if i < shots-1:
+                        stab_tmp = stab.clone()
+                    else:
+                        stab_tmp = stab
+                        
                     mval_list = []
-                    for q in c['qid']:
+                    for q in qcirc[-1]['qid']:
                         mval = stabilizer_measure(stab_tmp, q=q)
                         mval_list.append(mval)
                     mval_list_str = "".join(map(str, mval_list))
                     freq += Counter([mval_list_str])
+
+                    if qcirc[-1]['cid'] != None:
+                        for k,mval in enumerate(mval_list):
+                            cmem[qcirc[-1]['cid'][k]] = mval
+
+                    if i < shots-1:
+                        stab_tmp.free()
                 break
+            
         if only_one_measurement_end == True:
             break
 
@@ -129,7 +149,8 @@ def run_qlazy_stabilizer_simulator(stab, qcirc, cmem, shots=DEF_SHOTS):
             cmem = [0] * len(cmem)
             stab.set_all('Z')
 
-    if end_of_measurements > 0:
+    # if end_of_measurements > 0:
+    if measurement_cnt > 0:
         measured_qid = qcirc[end_of_measurements]['qid']
         result = {'measured_qid': measured_qid, 'frequency': freq}
     else:
