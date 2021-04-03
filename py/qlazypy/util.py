1357fbee7632a96ffb7ba2696ad2936e64e6951f
samn33 <saminriver33@gmail.com>
2021-04-03 16:01:30 +0900

    get_qgate_param_num

--- py/qlazypy/util.py
+++ py/qlazypy/util.py
@@ -150,11 +150,35 @@ def get_qgate_qubit_num(kind=None):
     elif (kind==CONTROLLED_X or kind==CONTROLLED_Y or kind==CONTROLLED_Z or
           kind==CONTROLLED_XR or kind==CONTROLLED_XR_ or kind==CONTROLLED_H or
           kind==CONTROLLED_S or kind==CONTROLLED_S_ or kind==CONTROLLED_T or
-          kind==CONTROLLED_T_ or kind==SWAP or kind==CONTROLLED_P or
+          kind==CONTROLLED_T_ or kind==SWAP_QUBITS or kind==CONTROLLED_P or
           kind==CONTROLLED_RX or kind==CONTROLLED_RY or kind==CONTROLLED_RZ or
           kind==CONTROLLED_U1 or kind==CONTROLLED_U2 or kind==CONTROLLED_U3 or
           kind==MEASURE_BELL):
         return 2
+    else:
+        raise QState_UnknownQgateKind()
+
+def get_qgate_param_num(kind=None):
+
+    if (kind==SHOW or kind==BLOCH or
+        kind==MEASURE or kind==MEASURE_X or kind==MEASURE_Y or kind==MEASURE_Z or kind==MEASURE_BELL or
+        kind==PAULI_X or kind==PAULI_Y or kind==PAULI_Z or
+        kind==ROOT_PAULI_X or kind==ROOT_PAULI_X_ or kind==HADAMARD or
+        kind==PHASE_SHIFT_S or kind==PHASE_SHIFT_S_ or kind==PHASE_SHIFT_T or kind==PHASE_SHIFT_T_ or
+        kind==CONTROLLED_X or kind==CONTROLLED_Y or kind==CONTROLLED_Z or
+        kind==CONTROLLED_XR or kind==CONTROLLED_XR_ or kind==CONTROLLED_H or
+        kind==CONTROLLED_S or kind==CONTROLLED_S_ or kind==CONTROLLED_T or kind==CONTROLLED_T_ or kind==SWAP_QUBITS):
+        return 0
+    elif (kind==PHASE_SHIFT or
+          kind==ROTATION_X or kind==ROTATION_Y or kind==ROTATION_Z or
+          kind==ROTATION_U1 or kind==CONTROLLED_P or
+          kind==CONTROLLED_RX or kind==CONTROLLED_RY or kind==CONTROLLED_RZ or
+          kind==CONTROLLED_U1):
+        return 1
+    elif (kind==ROTATION_U2 or kind==CONTROLLED_U2):
+        return 2
+    elif (kind==ROTATION_U3 or kind==CONTROLLED_U3):
+        return 3
     else:
         raise QState_UnknownQgateKind()
 
