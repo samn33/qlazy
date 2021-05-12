0536c4ecff37ac30c6309fee16a5fda50c9f50a2
samn33 <saminriver33@gmail.com>
2021-05-12 10:17:51 +0900

    fix some comments

--- py/qlazypy/DensOp.py
+++ py/qlazypy/DensOp.py
@@ -134,15 +134,15 @@ class DensOp(ctypes.Structure):
         method : func
             method (custum gate) to add.
 
-        Example
-        -------
-        def bell(self, q0, q1):
-            self.h(q0).cx(q0,q1)
-        ...
-        DensOp.add_method(bell)
-        qs = DensOp(qubit_num=2)
-        qs.bell(0,1)
-        ...
+        Examples
+        --------
+        >>> def bell(self, q0, q1):
+        >>>     self.h(q0).cx(q0,q1)
+        >>> ...
+        >>> DensOp.add_method(bell)
+        >>> qs = DensOp(qubit_num=2)
+        >>> qs.bell(0,1)
+        >>> ...
         
         """
         setattr(cls, method.__name__, method)
@@ -157,8 +157,8 @@ class DensOp(ctypes.Structure):
         methods : func, func, ...
             arguments of methods (custum gates) to add.
 
-        Example
-        -------
+        Examples
+        --------
         >>> def bell(self, q0, q1):
         >>>     self.h(q0).cx(q0,q1)
         >>>     return self
