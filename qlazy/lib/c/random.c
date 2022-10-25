20553398fa7c624aa19c4fefc7fd740d13387ea8
samn33 <saminriver33@gmail.com>
2022-10-25 09:02:31 +0900

    merge MT.h to random.c

--- qlazy/lib/c/random.c
+++ qlazy/lib/c/random.c
@@ -50,12 +50,12 @@
    - change the file name "mt19937ar.c" -> "MT.h"
 */
 
-///* Period parameters */  
-//#define MT_N 624
-//#define MT_M 397
-//#define MATRIX_A 0x9908b0dfUL   /* constant vector a */
-//#define UPPER_MASK 0x80000000UL /* most significant w-r bits */
-//#define LOWER_MASK 0x7fffffffUL /* least significant r bits */
+/* Period parameters */  
+#define MT_N 624
+#define MT_M 397
+#define MATRIX_A 0x9908b0dfUL   /* constant vector a */
+#define UPPER_MASK 0x80000000UL /* most significant w-r bits */
+#define LOWER_MASK 0x7fffffffUL /* least significant r bits */
 
 #include "qlazy.h"
 
